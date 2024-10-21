585f22561bda31f9be90516c2eeeb9aea8b1b035
Cheng Zhang <chengzhang98@outlook.com>
2024-10-21 20:38:07 +0100

    remove redundant ';'

--- Lab_2/bin2bcd_16.sv
+++ Lab_2/bin2bcd_16.sv
@@ -13,12 +13,12 @@
 
 module bin2bcd_16(
 	input  logic [15:0] x,			// value ot be converted
-	output logic [3:0]  BCD0, BCD1, BCD2, BCD3, BCD4;  // BCD digits
+	output logic [3:0]  BCD0, BCD1, BCD2, BCD3, BCD4   // BCD digits
 );
 	 // Concatenation of input and output
    logic [35:0] result;  // no of bits = no_of_bit of x + 4* no of digits
    integer i;
-   
+
    always_comb
         begin
             result[35:0] = 0;
@@ -28,15 +28,15 @@ module bin2bcd_16(
                     // Check if unit digit >= 5
                 if (result[19:16] >= 5)
                     result[19:16] = result[19:16] + 4'd3;
-                        
+
                 // Check if ten digita >= 5
                 if (result[23:20] >= 5)
                     result[23:20] = result[23:20] + 4'd3;
-                    
+
                     // CHeck if hundred digit >= 5
                 if (result[27:24] >= 5)
                     result[27:24] = result[27:24] + 4'd3;
-                        
+
                     // check if thousand digit > 5
                 if (result[31:28] >= 5)
                     result[31:28] = result[31:28] + 4'd3;
@@ -44,14 +44,13 @@ module bin2bcd_16(
                     // Shift everything left
                 result = result << 1;
             end
-            
+
             // Decode output from result
             BCD0 = result[19:16];
-                BCD1 = result[23:20];		
+                BCD1 = result[23:20];
                 BCD2 = result[27:24];
                 BCD3 = result[31:28];
                 BCD4 = result[35:32];
         end
-	
+
 endmodule
-	
